b5c76d7f67bb7196918803099b7f03c537ae8b33
pavisalavisa <kristicevic.antonio@gmail.com>
2021-05-15 08:53:38 +0200

    Add ping endpoint

--- src/DigitalSignature_v2/src/main/java/com/example/digitalsignatureapi/controllers/PingController.java
+++ src/DigitalSignature_v2/src/main/java/com/example/digitalsignatureapi/controllers/PingController.java
@@ -1,19 +1,13 @@
 package com.example.digitalsignatureapi.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingsController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+public class PingController {
+    
+    @GetMapping("/ping")
+    public String ping() {
+        return "PONG";
     }
 }
