09818e6165d7cb677b462b61a7bbccf852aa1a50
Antonio <kristicevic.antonio@gmail.com>
2021-06-12 14:15:22 +0200

    Close nav menu on logout

--- src/SignatureClientApp/src/components/Navbar.js
+++ src/SignatureClientApp/src/components/Navbar.js
@@ -4,7 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Switch from "@material-ui/core/Switch";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -39,6 +38,11 @@ export default () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handleLogout = () => {
+    handleClose();
+    logout();
+};
 
   return (
     <div className={classes.root}>
@@ -82,7 +86,7 @@ export default () => {
                 <MenuItem onClick={() => console.log("My account clicked!")}>
                   My account
                 </MenuItem>
-                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+                <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
               </Menu>
             </div>
           )}
