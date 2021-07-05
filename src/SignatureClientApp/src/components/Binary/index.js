d2924c4c32169250d3c54e8a59cd2a824aa1af34
Antonio Kristicevic <kristicevic.antonio@gmail.com>
2021-07-05 21:02:15 +0200

    Cleanup code and apply styling fixes

--- src/SignatureClientApp/src/components/Binary/index.js
+++ src/SignatureClientApp/src/components/Binary/index.js
@@ -1,53 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import BinarySelector from "./BinarySelector";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Grid, Backdrop, CircularProgress } from "@material-ui/core";
+import { Grid, Backdrop, CircularProgress } from "@material-ui/core";
 import VerificationResult from "../VerificationResult";
 import { Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  pdfPreviewContainer: {
-    height: "93.5vh",
-    overflow: "scroll",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  spinner: {
-    margin: theme.spacing(3, 0, 2),
-  },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
     color: "#fff",
   },
-  actionGrid: {
-    padding: theme.spacing(4, 0, 0, 4),
+  selectorContainer: {
+    margin: theme.spacing(4, 0, 0),
   },
 }));
 
 function Binary(props) {
   const [selectedFile, setSelectedFile] = useState();
   const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
-  const [pdf, setPdf] = useState();
   const [verificationResult, setVerificationResult] = useState();
   const classes = useStyles();
 
-  // Preview PDF
-  useEffect(() => {
-    setPdf(selectedFile);
-    setVerificationResult(null);
-  }, [selectedFile]);
-
   return (
-    <Grid container direction="row" justify="space-between">
+    <Grid container direction="row" justify="space-between" spacing={4}>
       <Backdrop className={classes.backdrop} open={isLoadingSubmit}>
         <CircularProgress color="inherit" />
       </Backdrop>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={classes.selectorContainer}>
         <BinarySelector
           setIsLoadingSubmit={setIsLoadingSubmit}
           setVerificationResult={setVerificationResult}
@@ -56,7 +35,9 @@ function Binary(props) {
           isLoadingSubmit={isLoadingSubmit}
         />
       </Grid>
-      <Divider />
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
       <Grid item xs={12}>
         <VerificationResult verificationResult={verificationResult} />
       </Grid>
