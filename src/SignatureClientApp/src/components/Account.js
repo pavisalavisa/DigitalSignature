c6554dfaac53655deb85d160fcb0e4454073724c
Antonio <kristicevic.antonio@gmail.com>
2021-07-14 18:42:55 +0200

    Implement personal information updating

--- src/SignatureClientApp/src/components/Account.js
+++ src/SignatureClientApp/src/components/Account.js
@@ -1,14 +1,15 @@
 import { Button, Typography } from "@material-ui/core";
-import React, { useContext, useEffect, useState } from "react";
-import { getPersonalInfo } from "../services/userManagementService";
+import React, { useEffect, useState } from "react";
+import { getPersonalInfo, updatePersonalInfo } from "../services/userManagementService";
 import { Divider } from "@material-ui/core";
 import { CircularProgress } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
+import { withSnackbar } from "./Snackbar";
 
 const useStyles = makeStyles((theme) => ({
   accountContainer: {
-    margin: theme.spacing(5),
+    margin: theme.spacing(4),
     "& > div": {
       marginTop: theme.spacing(3),
       "& > *": {
@@ -23,16 +24,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Account() {
+function Account({snackbarShowMessage}) {
   const [accountInfo, setAccountInfo] = useState();
   const { accountContainer, buttonGroup } = useStyles();
+  const [isDirty, setIsDirty] = useState(false);
+
+  async function fetchAndAssign() {
+    const personalInfo = await getPersonalInfo();
+    console.log(personalInfo);
+    setAccountInfo(personalInfo);
+    setIsDirty(false);
+  }
+
+  async function saveChanges(){
+    await updatePersonalInfo(accountInfo);
+    snackbarShowMessage("Account updated")
+    setIsDirty(false);
+  }
 
   useEffect(() => {
-    async function fetchAndAssign() {
-      const personalInfo = await getPersonalInfo();
-      setAccountInfo(personalInfo);
-    }
-
     fetchAndAssign();
   }, []);
 
@@ -62,41 +72,45 @@ function Account() {
             <div>
               <Typography color="textSecondary">Email</Typography>
               <TextField
-                value={accountInfo.email}
-                onChange={(e) =>
-                  setAccountInfo({ ...accountInfo, email: e.target.value })
-                }
+                value={accountInfo.email || ""}
+                onChange={(e) => {
+                  setAccountInfo({ ...accountInfo, email: e.target.value });
+                  setIsDirty(true);
+                }}
               />
             </div>
             <div>
               <Typography color="textSecondary">First name</Typography>
               <TextField
-                value={accountInfo.firstName}
-                onChange={(e) =>
-                  setAccountInfo({ ...accountInfo, firstName: e.target.value })
-                }
+                value={accountInfo.firstName || ""}
+                onChange={(e) => {
+                  setAccountInfo({ ...accountInfo, firstName: e.target.value });
+                  setIsDirty(true);
+                }}
               />
             </div>
             <div>
               <Typography color="textSecondary">Last name</Typography>
               <TextField
-                value={accountInfo.lastName}
-                onChange={(e) =>
-                  setAccountInfo({ ...accountInfo, lastName: e.target.value })
-                }
+                value={accountInfo.lastName || ""}
+                onChange={(e) => {
+                  setAccountInfo({ ...accountInfo, lastName: e.target.value });
+                  setIsDirty(true);
+                }}
               />
             </div>
             <div>
               <Typography color="textSecondary">Organization name</Typography>
               <TextField
                 size="small"
-                value={accountInfo.organizationName}
-                onChange={(e) =>
+                value={accountInfo.organizationName || ""}
+                onChange={(e) => {
                   setAccountInfo({
                     ...accountInfo,
                     organizationName: e.target.value,
-                  })
-                }
+                  });
+                  setIsDirty(true);
+                }}
               />
             </div>
           </>
@@ -117,13 +131,13 @@ function Account() {
         <Divider />
       </div>
       <div className={buttonGroup}>
-        <Button color="primary" variant="contained">
+        <Button disabled={!isDirty} color="primary" variant="contained" onClick={()=> saveChanges()}>
           Save
         </Button>
-        <Button>Cancel</Button>
+        <Button onClick={() => fetchAndAssign()}>Cancel</Button>
       </div>
     </div>
   );
 }
 
-export default Account;
+export default withSnackbar(Account);
