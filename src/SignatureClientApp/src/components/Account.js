ad1969b8b7a4fd5e8fa91ac22447b98bc0237863
Antonio <kristicevic.antonio@gmail.com>
2021-07-14 18:09:43 +0200

    Add style to account screen

--- src/SignatureClientApp/src/components/Account.js
+++ src/SignatureClientApp/src/components/Account.js
@@ -4,9 +4,29 @@ import { getPersonalInfo } from "../services/userManagementService";
 import { Divider } from "@material-ui/core";
 import { CircularProgress } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  accountContainer: {
+    margin: theme.spacing(5),
+    "& > div": {
+      marginTop: theme.spacing(3),
+      "& > *": {
+        marginTop: theme.spacing(2),
+      },
+    },
+  },
+  buttonGroup: {
+    "& > button:first-child": {
+      marginRight: theme.spacing(2),
+    },
+  },
+}));
 
 function Account() {
   const [accountInfo, setAccountInfo] = useState();
+  const { accountContainer, buttonGroup } = useStyles();
+
   useEffect(() => {
     async function fetchAndAssign() {
       const personalInfo = await getPersonalInfo();
@@ -17,64 +37,90 @@ function Account() {
   }, []);
 
   return (
-    <div>
-      <Typography variant="h3">My account</Typography>
-      <Typography variant="subtitle2" color="textSecondary">
-        Make changes to your account
-      </Typography>
-      <Divider />
+    <div className={accountContainer}>
       <div>
-        <Typography variant="h4">Personal information</Typography>
-        <Typography variant="subtitle2" color="textSecondary">
-          Make changes to your personal information
-        </Typography>
+        <div>
+          <Typography variant="h3">My account</Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            Make changes to your account
+          </Typography>
+        </div>
+        <Divider />
+      </div>
+      <div>
+        <div>
+          <Typography variant="h4">Personal information</Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            Make changes to your personal information
+          </Typography>
+        </div>
+
         {!accountInfo ? (
           <CircularProgress color="inherit" />
         ) : (
           <>
-            <Typography>Email</Typography>
-            <TextField
-              value={accountInfo.email}
-              onChange={(e) =>
-                setAccountInfo({ ...accountInfo, email: e.target.value })
-              }
-            />
-            <Typography>First name</Typography>
-            <TextField
-              value={accountInfo.firstName}
-              onChange={(e) =>
-                setAccountInfo({ ...accountInfo, firstName: e.target.value })
-              }
-            />
-            <Typography>Last name</Typography>
-            <TextField
-              value={accountInfo.lastName}
-              onChange={(e) =>
-                setAccountInfo({ ...accountInfo, lastName: e.target.value })
-              }
-            />
-            <Typography>Organization name</Typography>
-            <TextField
-              size="small"
-              value={accountInfo.organizationName}
-              onChange={(e) =>
-                setAccountInfo({
-                  ...accountInfo,
-                  organizationName: e.target.value,
-                })
-              }
-            />
+            <div>
+              <Typography color="textSecondary">Email</Typography>
+              <TextField
+                value={accountInfo.email}
+                onChange={(e) =>
+                  setAccountInfo({ ...accountInfo, email: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <Typography color="textSecondary">First name</Typography>
+              <TextField
+                value={accountInfo.firstName}
+                onChange={(e) =>
+                  setAccountInfo({ ...accountInfo, firstName: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <Typography color="textSecondary">Last name</Typography>
+              <TextField
+                value={accountInfo.lastName}
+                onChange={(e) =>
+                  setAccountInfo({ ...accountInfo, lastName: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <Typography color="textSecondary">Organization name</Typography>
+              <TextField
+                size="small"
+                value={accountInfo.organizationName}
+                onChange={(e) =>
+                  setAccountInfo({
+                    ...accountInfo,
+                    organizationName: e.target.value,
+                  })
+                }
+              />
+            </div>
           </>
         )}
+        <Divider />
       </div>
-      <Divider />
       <div>
-        <Typography variant="h4">Certificate</Typography>
-        <Typography variant="subtitle2" color="textSecondary">
-          Import/Export certificate used for digital signature services
-        </Typography>
-        <Button>Import</Button>
-        <Button>Export</Button>
+        <div>
+          <Typography variant="h4">Certificate</Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            Import/Export certificate used for digital signature services
+          </Typography>
+        </div>
+        <div className={buttonGroup}>
+          <Button variant="outlined">Import</Button>
+          <Button variant="outlined">Export</Button>
+        </div>
+        <Divider />
+      </div>
+      <div className={buttonGroup}>
+        <Button color="primary" variant="contained">
+          Save
+        </Button>
+        <Button>Cancel</Button>
       </div>
     </div>
   );
