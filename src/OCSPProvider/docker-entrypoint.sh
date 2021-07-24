f6708a55974e37c7fc0b3297310f512154f8a63a
Antonio <kristicevic.antonio@gmail.com>
2021-07-24 09:38:11 +0200

    Use OCSP cert to sign OCSP responses

--- src/OCSPProvider/docker-entrypoint.sh
+++ src/OCSPProvider/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 openssl ocsp -port 0.0.0.0:2560 -text -sha256 \
       -index /root/ca/intermediate/index.txt \
       -CA /root/ca/intermediate/certs/ca-chain.cert.pem \
-      -rkey /root/ca/intermediate/private/intermediate.key.pem \
-      -rsigner /root/ca/intermediate/certs/intermediate.cert.pem
+      -rkey /root/ca/intermediate/private/ocsp.key.pem \
+      -rsigner /root/ca/intermediate/certs/ocsp.cert.pem
